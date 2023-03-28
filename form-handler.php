2b29cec9894727ef82260b7216db22546c9db06b
akdevlabs <116324447+akdevlabs@users.noreply.github.com>
2023-03-27 23:46:19 -0600

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,8 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$Date =  $_Post['date'];
+$Phone =  $_Post['phone'];
+$choose = $_Post['choose'];
+$message =  $_Post['message'];
+?>
